1f0a9ae368435c146fc9d80e00a599af2a4cc5ef
Spencer Herms <slherms@gmail.com>
2018-04-06 18:54:25 -0600

    Add imports and GET route to server

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 const express = require('express');
-
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const bodyParser = require('body-parser');
+const database = require('knex')(configuration);
 
+app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Zelda Cookbook Backend';
 
-app.get('/', (request, response) => {
-  response.send('Yea buddy!!');
+app.get('/ingredients', (request, response) => {
+  database('ingredients')
+    .select()
+    .then(items => {
+      if (items.length) {
+        response.status(200).json(items);
+      } else {
+        response.status(404).json({
+          error: 'ingredients not found'
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
 });
+
 
 app.listen(app.get('port'), () => {
   // eslint-disable-next-line
-  console.log('Zelda Cookbook is running.');
+  console.log(`${app.locals.title} server is running on ${app.get('port')}.`);
 });
+
+module.exports = app;
