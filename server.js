76466b9d31b425fdc8a7efc7eca5da8f7b13167d
Spencer Herms <slherms@gmail.com>
2018-04-06 19:54:52 -0600

    adds `ingredients/:id` `/api/v1/recipes`  `/api/v1/recipes/:id` routes

--- server.js
+++ server.js
@@ -9,15 +9,68 @@ app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Zelda Cookbook Backend';
 
-app.get('/ingredients', (request, response) => {
+app.get('/api/v1/ingredients', (request, response) => {
   database('ingredients')
     .select()
-    .then(items => {
-      if (items.length) {
-        response.status(200).json(items);
+    .then(ingredients => {
+      if (ingredients.length) {
+        response.status(200).json(ingredients);
       } else {
         response.status(404).json({
           error: 'ingredients not found'
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/ingredients/:id', (request, response) => {
+  database('ingredients')
+    .where('id', request.params.id)
+    .select()
+    .then(ingredient => {
+      if (ingredient.length) {
+        response.status(200).json(ingredient);
+      } else {
+        response.status(404).json({
+          error: `Could not find ingredient with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/recipes', (request, response) => {
+  database('recipes')
+    .select()
+    .then(recipes => {
+      if (recipes.length) {
+        response.status(200).json(recipes);
+      } else {
+        response.status(404).json({
+          error: 'recipes not found'
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/recipes/:id', (request, response) => {
+  database('recipes')
+    .where('id', request.params.id)
+    .select()
+    .then(recipe => {
+      if (recipe.length) {
+        response.status(200).json(recipe);
+      } else {
+        response.status(404).json({
+          error: `Could not find recipe with id ${request.params.id}`
         });
       }
     })
