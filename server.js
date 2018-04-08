28339e40a470baf7c10156618bf29317f07c8d79
Alex Bonder <lexbonder@gmail.com>
2018-04-07 20:46:18 -0600

    Add fix for CORS error in server.js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const bodyParser = require('body-parser');
 const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Zelda Cookbook Backend';
 
