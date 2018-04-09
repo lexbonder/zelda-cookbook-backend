2421a2a5e1297e49f2c3cda80cf5ccaeab578e37
Alex Bonder <lexbonder@gmail.com>
2018-04-09 15:24:58 -0600

    Edit knexfile

--- knexfile.js
+++ knexfile.js
@@ -28,7 +28,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: `${process.env.DATABASE_URL} ?ssl=true`,
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
       directory: './db/migrations',
     },
