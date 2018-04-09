0ec2e557c1cdab02ccbf4cacd3f4291010d71ab2
Alex Bonder <lexbonder@gmail.com>
2018-04-09 13:59:41 -0600

    import full database into development.
    Updated migration to take all columns

--- db/seeds/dev/cookbook.js
+++ db/seeds/dev/cookbook.js
@@ -1,78 +1,40 @@
+const ingredients = require('../../../data/ingredients-data.json')
+const recipes = require('../../../data/recipe-data.json')
+
 exports.seed = (knex, Promise) => {
   return knex('recipes')
-  .del()
-  .then(() => knex('ingredients').del())
-  .then(() => {
-    return Promise.all([
-      knex('ingredients')
-      .insert([
-        {
-          "category": "food",
-          "duration": "0:30",
-          "effect": null,
-          "hearts": "0.5",
-          "name": "Apple",
-          "resale": "3",
-          "type": "Fruit"
-        },
-        {
-          "category": "food",
-          "duration": "1:00",
-          "effect": null,
-          "hearts": "1",
-          "name": "Tabantha Wheat",
-          "resale": "3",
-          "type": "Seasoning"
-        },
-        {
-          "category": "food",
-          "duration": "1:20",
-          "effect": null,
-          "hearts": "0",
-          "name": "Cane Sugar",
-          "resale": "3",
-          "type": "Seasoning"
-        },
-        {
-          "category": "food",
-          "duration": "1:20",
-          "effect": null,
-          "hearts": "0",
-          "name": "Goat Butter",
-          "resale": "3",
-          "type": "Seasoning"
-        }], 'id')
-      .then(ingredient => {
-        return knex('recipes')
-        .insert([
-          {
-            "category": "recipe",
-            "hearts": "3",
-            "ingredient1": ingredient[0],
-            "ingredient2": ingredient[1],
-            "ingredient3": ingredient[2],
-            "ingredient4": ingredient[3],
-            "ingredient5": null,
-            "name": "Apple Pie",
-            "notes": "Tabantha Wheat, Cane Sugar & Goat Butter obtainable in Rito Village.",
-            "resale": "30",
-            "type": "Restore Hearts"
-          },
-          {
-            "category": "recipe",
-            "hearts": "¾",
-            "ingredient1": ingredient[0],
-            "ingredient2": null,
-            "ingredient3": null,
-            "ingredient4": null,
-            "ingredient5": null,
-            "name": "Baked Apple",
-            "notes": "Open flame.",
-            "resale": "0",
-            "type": "Restore Hearts"
-          }
-        ])
-      }) 
-    ])
-  })
+    .del()
+    .then(() => knex('ingredients').del())
+    .then(() => {
+      return Promise.all([
+        knex('ingredients').insert(
+          ingredients, 'id'
+        )
+        .then(ingredient => {
+          return knex('recipes')
+          .insert(
+            recipes.map(recipe => {
+              let multipliers = ['x2', 'x3', 'x4', 'x5']
+              let ingredients = {}
+              let ingName;
+
+              for (let i = 1; i <= 5; i++) {
+                if (recipe[`ingredient${i}`]) {
+                  multipliers.forEach( item => {
+                    if (recipe[`ingredient${i}`].includes(item)) {
+                      ingName = recipe[`ingredient${i}`].slice(0, -3)
+                    } 
+                    ingredients[`ingredient${i}`] = knex('ingredients')
+                      .where('name', ingName || recipe[`ingredient${i}`])
+                      .select('id')
+                  })
+                  
+                } 
+              }
+              return {...recipe, ...ingredients}
+            })
+          )
+        }) 
+      ])
+    })
 };
