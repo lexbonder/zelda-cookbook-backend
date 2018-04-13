08fa3347953589240b0609a52827c6177c7be78a
Spencer Herms <slherms@gmail.com>
2018-04-12 21:28:33 -0600

    remove trailing commas

--- knexfile.js
+++ knexfile.js
@@ -1,41 +1,39 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'pg',
     connection: 'postgres://localhost/zelda_cookbook',
     migrations: {
-      directory: './db/migrations',
+      directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/dev',
+      directory: './db/seeds/dev'
     },
-    useNullAsDefault: true,
+    useNullAsDefault: true
   },
 
   test: {
     client: 'pg',
     connection: 'postgres://localhost/zelda_cookbook_test',
     migrations: {
-      directory: './db/migrations',
+      directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/test',
+      directory: './db/seeds/test'
     },
-    useNullAsDefault: true,
+    useNullAsDefault: true
   },
 
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
-      directory: './db/migrations',
+      directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/dev',
+      directory: './db/seeds/dev'
     },
-    useNullAsDefault: true,
-  },
-
+    useNullAsDefault: true
+  }
 };
